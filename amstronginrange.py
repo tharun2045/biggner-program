d09ad647efedba15f25b5f38c29fcd9b8f8db14c
tharun2045 <40885759+tharun2045@users.noreply.github.com>
2018-07-17 14:26:42 +0530

    Update amstronginrange.py

--- amstronginrange.py
+++ amstronginrange.py
@@ -1,13 +1,11 @@
-lower = int(raw_input())
-upper = int(raw_input())
-for num in range(lower, upper + 1):
-   order = len(str(num))
+lo,up=map(int,input().strip().split())
+for j in range(lo, up + 1):
+   order = len(str(j))
    sum = 0
-   temp = num
+   temp = j
    while temp > 0:
        digit = temp % 10
        sum += digit ** order
        temp //= 10
-
-   if num == sum:
-       print(num)
+   if j == sum:
+       print(j,end=" ")
