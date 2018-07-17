f86f7867d657ef305ee94aaa22f7067899acfd0c
tharun2045 <40885759+tharun2045@users.noreply.github.com>
2018-07-17 11:22:56 +0530

    Create primeinrange.py

---
+++ primeinrange.py
@@ -0,0 +1,8 @@
+lo,up=map(int,input().strip().split())
+for num in range(lo+1,up):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+        print(num,end=" ")
